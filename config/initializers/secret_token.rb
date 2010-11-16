8f5bc50232b63c40fda346ac9e30098a75c0430b
David Matthews <predsfan26@gmail.com>
2010-11-15 22:21:53 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moap::Application.config.secret_token = '50c9bb447174a880729227b8150898e621594241e6aacbba05daf532778add2646de63ff5e4cb6532fbd3d6e1ac74d52193680586e6d73a0552f0f6914fbb1a0'
